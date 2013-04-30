522732ead24e45f57038b7e1831f5f6d6a8590be
Glenn.latomme <glenn.latomme@gmail.com>
2013-04-30 15:40:33 +0200

    404 update

--- app/app.php
+++ app/app.php
@@ -3,11 +3,9 @@
 // Bootstrap
 require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
-// Use Request from Symfony Namespace
-$app->error(function (\Exception $e, $code) {
+$app->error(function (\Exception $e, $code) use ($app) {
 	if ($code == 404) {
-		//return $app['twig']->render('errors/404.twig', array('error' => $e->getMessage()));
-            return '404 - Not Found! // ' . $e->getMessage();
+		return $app['twig']->render('errors/404.twig', array('error' => $e->getMessage()));
 	} else {
 		return 'Shenanigans! Something went horribly wrong // ' . $e->getMessage();
 	}
